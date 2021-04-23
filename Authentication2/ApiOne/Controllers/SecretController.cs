271d36dd589a27690b155a417d94554ab27a7ed0
Nikita Grebnev <n.a.grebnev@gmail.com>
2021-04-22 22:19:41 -0400

    mockup get all get one

--- Authentication2/ApiOne/Controllers/SecretController.cs
+++ Authentication2/ApiOne/Controllers/SecretController.cs
@@ -5,11 +5,34 @@ namespace ApiOne.Controllers
 {
     public class SecretController : Controller
     {
+        private string[] _ds = { "{id: 0, name: \"foo\"}", "{id: 1, name: \"bar\"}" };
+
         [Route("/secret")]
         [Authorize]
         public string Index()
         {
             return "secret message from ApiOne";
         }
+
+        [Route("/getall")]
+        public IActionResult GetAll()
+        {
+            var ds = _ds;
+            return Ok(new
+            {
+                message = ds,
+            });
+        }
+
+        [Route("/getone/{ds_id}")]
+        public IActionResult GetOne(int ds_id)
+        {
+            var ds = _ds;
+            return Ok(new
+            {
+                message = ds[ds_id],
+            });
+        }
+
     }
 }
